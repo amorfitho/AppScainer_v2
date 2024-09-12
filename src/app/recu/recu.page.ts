fafc185902ee4d2008c0021ca146a28e66eaef4a
Fabian <119624256+amorfitho@users.noreply.github.com>
2024-09-12 20:36:40 -0300

    avanze javascript
    java scrip terminado (sin prueba)

--- src/app/recu/recu.page.ts
+++ src/app/recu/recu.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, 
+  FormControl,
+  Validators,
+  FormBuilder 
+   } from '@angular/forms';
+import { AlertController, NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-recu',
@@ -7,9 +14,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecuPage implements OnInit {
 
-  constructor() { }
+  formularioRecu: FormGroup;
+
+  constructor(public fb: FormBuilder, public alertController: AlertController, public navControl:NavController) { 
+    this.formularioRecu = this.fb.group({
+      'nombre': new FormControl("",Validators.required),
+      'oldpassword': new FormControl("",Validators.required),
+      'password': new FormControl("",Validators.required),
+      'comfirmpassword': new FormControl("",Validators.required)
+    })
+
+  }
 
   ngOnInit() {
   }
+
+  async actulizar(){
+    const f = this.formularioRecu.value;
+
+    const usuario = JSON.parse(localStorage.getItem('usuario') || '{}');
+
+    localStorage.getItem('password');
+
+    if (usuario.nombre== f.nombre && usuario.password==f.oldpassword && f.password==f.comfirmpassword){
+      console.log('Actualizado');
+      localStorage.setItem('Actualizado','true');
+      localStorage.getItem('password');
+      this.navControl.navigateRoot('login');
+    }else{
+      const alert = await this.alertController.create({
+        header: 'Algo te falto',
+        message: 'Tienes que llenar todos los campos.',
+        buttons: ['Ta Bien']
+      })
+
+      await alert.present();
+      return;
+    }
+
+  }
+
+
 
 }
