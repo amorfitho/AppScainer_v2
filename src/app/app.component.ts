189a5e1832df2612435b2d58951e31d654d54899
Fabian <119624256+amorfitho@users.noreply.github.com>
2024-09-13 19:59:25 -0300

    agragr animacionse movimiento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  query,
+  group
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    // Definir la animación para las transiciones entre páginas
+    trigger('routeAnimations', [
+      transition('* <=> *', [
+        style({ transform: 'translateX(100%)', opacity: 0 }),
+        animate('500ms ease-in-out', style({ transform: 'translateX(0)', opacity: 1 }))
+      ]),
+    ])
+    
+  ]
 })
 export class AppComponent {
-  constructor() {}
+  // Este método prepara la animación
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
 }
